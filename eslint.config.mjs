1bc5e604b49bc68ea55bd0a5a776d526948ef60c
sikandarsubhani <sikandar@jfreaks.com>
2026-03-07 02:31:39 +0500

    chore: add Prettier configuration and refactor ESLint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,45 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import { defineConfig } from 'eslint/config';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+export default defineConfig([
+  {
+    ignores: [
+      '*.css',
+      '.next/',
+      'node_modules/',
+      'out/',
+      'build/',
+      'next.config.ts',
+    ],
+    extends: [...nextCoreWebVitals],
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { varsIgnorePattern: '__dirname', argsIgnorePattern: '^_' },
+      ],
+      'no-unreachable': 'error',
+      'no-self-assign': 'error',
+      'no-empty': 'error',
+      'no-empty-function': 'error',
+      'require-await': 'error',
+      'prefer-const': 'error',
+      yoda: 'error',
+      'react/function-component-definition': [
+        'error',
+        { namedComponents: 'function-declaration' },
+      ],
+      'react-hooks/rules-of-hooks': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+    },
+  },
+
+  {
+    files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+    rules: {},
+  },
 ]);
-
-export default eslintConfig;
